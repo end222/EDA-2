d70775cedbced023fbe774b65b7417f836468aed
Naval Group <naval@europe.com>
2017-12-14 20:16:27 +0100

    Fichero cmt.h concluido, todas las funciones implementadas. Pendiente de revisión.

--- cmt/cmt.h
+++ cmt/cmt.h
@@ -4,7 +4,6 @@
  * Daniel Naval
  * Fichero: cmt.h
  */
-
 #include <iostream>
 #include <string>
 #include <queue>
@@ -19,17 +18,16 @@ template <typename T1, typename T2, typename T3> struct terna;
 template <typename T1, typename T2, typename T3> struct cmt;
 template <typename T1, typename T2, typename T3> struct Nodo;
 // definir operaciones de agrupacion
-
 template <typename T1, typename T2, typename T3> void crear(cmt<T1, T2, T3>& c);
 template <typename T1, typename T2, typename T3> bool esVacio(cmt<T1, T2, T3>& c);
 template <typename T1, typename T2, typename T3> bool existeClave(cmt<T1, T2, T3>& c, T1& k);
-template <typename T1, typename T2, typename T3> void introducir(cmt<T1, T2, T3>& c, T1& k, T2& v);
+template <typename T1, typename T2, typename T3> bool introducir(cmt<T1, T2, T3>& c, T1& k, T2& v);
 
 template <typename T1, typename T2, typename T3> bool tieneTiempo(cmt<T1, T2, T3>& c, T1& k);
 template <typename T1, typename T2, typename T3> bool obtenerDato(cmt<T1, T2, T3>& c, T1& k, T2& v);
-template <typename T1, typename T2, typename T3> bool obtenerTiempo(cmt<T1, T2, T3>& c, T1& k, T3& t);
+template <typename T1, typename T2, typename T3> int obtenerTiempo(cmt<T1, T2, T3>& c, T1& k, T3& t);
 template <typename T1, typename T2, typename T3> bool marcarTiempo(cmt<T1, T2, T3>& c, T1& k, T3& t);
-template <typename T1, typename T2, typename T3> void borrar(cmt<T1, T2, T3>& c, T1& k);
+template <typename T1, typename T2, typename T3> int borrar(cmt<T1, T2, T3>& c, T1& k);
 template <typename T1, typename T2, typename T3> int tamano(cmt<T1, T2, T3>& c);
 template <typename T1, typename T2, typename T3> string listar(cmt<T1, T2, T3>& c);
 // ITERADORES
@@ -55,13 +53,13 @@ struct cmt {
     friend void crear<T1, T2, T3>(cmt<T1, T2, T3>& c);
     friend bool esVacio<T1, T2, T3>(cmt<T1, T2, T3>& c);
     friend bool existeClave<T1, T2, T3>(cmt<T1, T2, T3>& c, T1& k);
-    friend void introducir<T1, T2, T3>(cmt<T1, T2, T3>& c, T1& k, T2& v);
+    friend bool introducir<T1, T2, T3>(cmt<T1, T2, T3>& c, T1& k, T2& v);
 
     friend bool tieneTiempo<T1, T2, T3>(cmt<T1, T2, T3>& c, T1& k);
     friend bool obtenerDato<T1, T2, T3>(cmt<T1, T2, T3>& c, T1& k, T2& v);
-    friend bool obtenerTiempo<T1, T2, T3>(cmt<T1, T2, T3>& c, T1& k, T3& t);
+    friend int obtenerTiempo<T1, T2, T3>(cmt<T1, T2, T3>& c, T1& k, T3& t);
     friend bool marcarTiempo<T1, T2, T3>(cmt<T1, T2, T3>& c, T1& k, T3& t);
-    friend void borrar<T1, T2, T3>(cmt<T1, T2, T3>& c, T1& k);
+    friend int borrar<T1, T2, T3>(cmt<T1, T2, T3>& c, T1& k);
     friend int tamano<T1, T2, T3>(cmt<T1, T2, T3>& c);
     friend string listar<T1, T2, T3>(cmt<T1, T2, T3>& c);
     // ITERADORES
@@ -87,7 +85,7 @@ bool esVacio(cmt<T1,T2,T3>& c){
 	return c==nullptr;	
 }
 template <typename T1, typename T2, typename T3>
-bool existe_aux (Nodo<T1,T2,T3>* entrada, Nodo<T1,T2,T3>& salida, const T1& clave){
+bool existe_aux (Nodo<T1,T2,T3>* entrada, Nodo<T1,T2,T3>* salida, const T1& clave){
 	if (entrada==nullptr){
 		return true;
 	}
@@ -108,17 +106,40 @@ bool existe_aux (Nodo<T1,T2,T3>* entrada, Nodo<T1,T2,T3>& salida, const T1& clav
 template <typename T1, typename T2, typename T3>
 bool existeClave(cmt<T1, T2, T3>& c, T1& k){
 	Nodo<T1,T2,T3>* aux; 
-	return (c.abb,aux,k);
+	return (existe_aux(c.abb,aux,k));
 }
 
 template <typename T1, typename T2, typename T3>
-void introducir_aux (Nodo<T1,T2,T3>* entrada, Nodo<T1,T2,T3>& salida){
-	
+bool introducir_aux (Nodo<T1,T2,T3>* entrada, T1& k, T2& v){
+	if (entrada==nullptr){
+		entrada=new typename cmt<T1,T2,T3>::Nodo;
+		entrada->dato.k=k;
+		entrada->dato.v=v;
+		entrada->der=nullptr;
+		entrada->izq=nullptr;
+		return 0;
+	}
+	else if (entrada->dato.k==k){
+		entrada->dato.v=v;
+		return 1;
+	}
+	else{
+		if(k<=entrada->dato.k){
+			introducir_aux(entrada->izq);
+		}
+		else{
+			introducir_aux(entrada->der);
+		}
+	}
 }
 
 template <typename T1, typename T2, typename T3>
-void introducir(cmt<T1, T2, T3>& c, T1& k, T2& v){
-	introducir_aux(c->abb);
+bool introducir(cmt<T1, T2, T3>& c, T1& k, T2& v){
+	bool control;
+	if (control=introducir_aux(c.abb,k,v)){
+		c.numElementos++;
+	}
+	return control;
 }
 template <typename T1, typename T2, typename T3>
 void inOrden(Nodo<T1,T2,T3>* entrada, queue <terna<T1,T2,T3>* > l){
@@ -135,6 +156,130 @@ void inOrden(Nodo<T1,T2,T3>* entrada, queue <terna<T1,T2,T3>* > l){
 	}
 }
 
+template <typename T1, typename T2, typename T3> 
+bool tieneTiempo(cmt<T1, T2, T3>& c, T1& k){
+	Nodo<T1, T2, T3>* testigo;
+	existe_aux(c.abb,testigo,k);
+	if (testigo->dato.hayTiempo==true){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+template <typename T1, typename T2, typename T3> 
+bool obtenerDato(cmt<T1, T2, T3>& c, T1& k, T2& v){
+	bool fase;
+	Nodo<T1, T2, T3>* testigo;
+	fase = existe_aux(c.abb,testigo,k);
+	v=testigo->dato;
+	return fase;
+}
+
+template <typename T1, typename T2, typename T3>
+int obtenerTiempo(cmt<T1, T2, T3>& c, T1& k, T3& t){
+	bool fase; //0EXISTE LA CLAVE, 1NOEXISTE LA CLAVE
+	Nodo<T1, T2, T3>* testigo;
+	fase = existe_aux(c.abb,testigo,k);
+	if (fase){
+		if (testigo->dato.hayTiempo==true){
+			t=testigo->dato.t;
+			return 0; //Ha ido todo correctamente :)
+		}
+		else{
+			return 1; // La clave no tiene tiempo :(
+		}
+	}
+	else{
+		return 2; //La clave no existe :(
+	}
+}
+template <typename T1, typename T2, typename T3> 
+bool marcarTiempo(cmt<T1, T2, T3>& c, T1& k, T3& t){
+	bool fase; //0EXISTE LA CLAVE, 1NOEXISTE LA CLAVE
+	Nodo<T1, T2, T3>* testigo;
+	fase = existe_aux(c.abb,testigo,k);
+	if (fase){
+		testigo->dato.t=t;
+		return 0; //Ha ido todo correctamente :)
+	}
+	
+	else{
+		return 1; //La clave no existe :(
+	}
+}
+template <typename T1, typename T2, typename T3> 
+void max(Nodo<T1,T2,T3>* entrada, T1& clave){
+	if (entrada.der==nullptr){
+		clave=entrada->dato.k;
+	}
+	else{
+		max(entrada->der,clave);
+	}
+}
+template <typename T1, typename T2, typename T3> 
+int borrar_aux (Nodo<T1,T2,T3>* entrada, T1& k){
+	Nodo<T1,T2,T3>* aux;
+	if (entrada==nullptr){
+		if (k<(entrada->dato.k)){
+			borrar_aux(entrada->izq,k);
+		}
+		else if (k>(entrada->dato.k)){
+			borrar_aux(entrada->der,k);
+		}
+		else if (k==(entrada->dato.k)){
+			if (entrada->izq==nullptr){
+				aux=entrada;
+				entrada=entrada->der;
+				delete (aux);
+				return 0; //Todo correcto
+			}
+			else{
+				entrada->dato.k=max(entrada->izq);
+				borrar(entrada->izq,entrada->dato.k);
+			}
+		}
+	
+	}
+	else{
+		return 1; // Problemas al borrar, árbol vacio
+	}
+}
+template <typename T1, typename T2, typename T3> 
+int borrar(cmt<T1, T2, T3>& c, T1& k){
+	int control;
+	if (borrar=borrar_aux(c->abb)==0){
+		c.numElementos--;
+	}
+	return control;
+}
+template <typename T1, typename T2, typename T3> 
+int tamano(cmt<T1, T2, T3>& c){
+	return c.numElementos;
+}
+template <typename T1, typename T2, typename T3> 
+string listar(cmt<T1, T2, T3>& c){
+	string corcheteA="[";
+	string corcheteC="]";
+	string dosPuntos=":";
+	string escribe;
+	T1 clave;
+	T2 valor;
+	T3 tiempo;
+	iniciarIterador(c);
+	
+	while (existeSiguiente(c)){
+		siguiente(c,clave,valor,tiempo);
+		if(tieneTiempo(c,clave)){
+			escribe=corcheteA+generaCadena(clave)+dosPuntos+dosPuntos+dosPuntos+generaCadena(tiempo)+'\n'+generaCadena(valor)+corcheteC+'\n';
+		}
+		else{
+			escribe=corcheteA+generaCadena(clave)+dosPuntos+dosPuntos+dosPuntos+'-'+'\n'+generaCadena(valor)+corcheteC+'\n';
+		}
+	}
+	return escribe;
+}
 template <typename T1, typename T2, typename T3>
 void iniciarIterador(cmt<T1, T2, T3>& c){
 	inOrden(c,c.l);
@@ -148,9 +293,13 @@ bool siguiente(cmt<T1, T2, T3>& c, T1& k, T2& v, T3& t){
 	if (c.iterador->dato.hayTiempo){
 		t=c.iterador->dato.t;
 	}
-	if (existeSiguiente(c){
+	if (existeSiguiente(c)){
 		c.iterador=c.l.pop();
-	} 
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 
 template <typename T1, typename T2, typename T3> 
