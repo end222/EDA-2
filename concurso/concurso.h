381388e8c309441d1f7949abf04d2356eb00deb1
Pablo <pabloorduna98@gmail.com>
2017-12-18 14:47:03 +0100

    Cosas

--- concurso/concurso.h
+++ concurso/concurso.h
@@ -32,7 +32,7 @@ int borrarPregunta(concurso& c, int clave);
 int totalPreguntas(concurso& c);
 bool existePregunta(concurso& c, int clave);
 bool obtenerPregunta(concurso& c, int clave, pregunta& p);
-int preguntaUtilizada(concurso& c, int clave);
+bool preguntaUtilizada(concurso& c, int clave);
 int obtenerUltimoUsoPregunta(concurso& c, int clave, instante& t);
 bool marcarPregunta(concurso& c, int clave, instante t);
 bool iniciarInscripcion(concurso& c);
@@ -63,7 +63,7 @@ struct concurso{
 	friend int totalPreguntas(concurso& c);
 	friend bool existePregunta(concurso& c, int clave);
 	friend bool obtenerPregunta(concurso& c, int clave, pregunta& p);
-	friend int preguntaUtilizada(concurso& c, int clave);
+	friend bool preguntaUtilizada(concurso& c, int clave);
 	friend int obtenerUltimoUsoPregunta(concurso& c, int clave, instante& t);
 	friend bool marcarPregunta(concurso& c, int clave, instante t);
 	friend bool iniciarInscripcion(concurso& c);
@@ -88,8 +88,8 @@ struct concurso{
 		bool enDoc;
 		bool enInsc;
 		bool enJ;
-		ronda<T1,T2> ronda;
-		cmt<T1,T2,T3> cmt;
+		ronda<string,participante> round;
+		cmt<int,pregunta,instante> grupo;
 };
 
 void crearConcurso(int PMax, int FMax, int numGmax, concurso& c){
@@ -99,6 +99,8 @@ void crearConcurso(int PMax, int FMax, int numGmax, concurso& c){
 	c.enDoc = true;
 	c.enInsc = false;
 	c.enJ = false;
+	crear(c.round);
+	crear(c.grupo);
 }
 
 int maximoPasesPermitidos(concurso& c){
@@ -126,75 +128,122 @@ bool enJuego(concurso& c){
 }
 
 int anadirPregunta(concurso& c, int clave, pregunta p){
-	return annadir(c.ronda,clave,pregunta);
+	if(enDocumentacion(c)){
+		return introducir(c.grupo,clave,pregunta);
+	}
+	else{
+		return 2;
+	}
 }
 
 int borrarPregunta(concurso& c, int clave){
-	return quitar(c.ronda, clave);
+	if(enDocumentacion(c)){
+		return borrar(c.grupo, clave);
+	}
+	else{
+		return 2;
+	}
 }
 
 int totalPreguntas(concurso& c){
-	return cardinal(c.ronda);
+	return tamano(c.grupo);
 }
 
 bool existePregunta(concurso& c, int clave){
-	return pertenece(c.ronda, clave);
+	return existeClave(c.grupo, clave);
 }
 
 bool obtenerPregunta(concurso& c, int clave, pregunta& p){
-	return obtenerValor(c.ronda, clave, p);
+	return obtenerDato(c.grupo, clave, p);
 }
 
-int preguntaUtilizada(concurso& c, int clave){
-
+bool preguntaUtilizada(concurso& c, int clave){
+	return tieneTiempo(c.grupo, clave);
 }
 
 int obtenerUltimoUsoPregunta(concurso& c, int clave, instante& t){
-
+	/*
+	 * 0: correcto
+	 * 1: no tiene tiempo
+	 * 2: no existe
+	 */
+	return obtenerTiempo(concurso& c, clave, t);
 }
 
 bool marcarPregunta(concurso& c, int clave, instante t){
-
+	return marcarTiempo(c.grupo, clave, t);
 }
 
 bool iniciarInscripcion(concurso& c){
-
+	if(!enDocumentacion(c) && totalPreguntas(c)==0){
+		c.enDoc = false;
+		c.enInsc = true;
+		return true;
+	else{
+		return false;
+	}
 }
 
 int anadirConcursante(concurso& c, string IDconcursante, participante p){
-
+	return annadir(c.round, IDconcursante, p);
 }
 
 int borrarConcursante(concurso& c, string IDconcursante){
-
+	return quitar(c.round, IDconcursante);
 }
 
 bool existeConcursante(concurso& c, string IDconcursante){
-
+	return pertenece(c.round, IDconcursante);
 }
 
 bool obtenerInfoConcursante(concurso& c, string IDconcursante, participante p){
-
+	return obtenerValor(c.round, IDconcursante, p);
 }
 
 int totalConcursantes(concurso& c){
-
+	return cardinal(c.round);
 }
 
 bool iniciarJuego(concurso c){
-
+	if(enInscripcion(c) && totalConcursantes(c)>máximoNúmeroGanadores(c)){
+		c.enJ = true;
+		c.enInsc = false;
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 
 bool existeConcursanteActual(concurso& c){
-
+	if(enjuego(c) && totalConcursantes(c)>0){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 
 int obtenerConcursanteActual(concurso& c, string& concursante){
-
+	if(enJuego(c)){
+		if(totalConcursantes(c)>0){
+			obtenerCandidatoSuClave(c.round,concursante);
+			return 2; // Se consigue obtener
+		else{
+			return 1; // No hay concursantes
+		}
+	}
+	else{
+		return 0; // No esta en juego
 }
 
 bool hayGanadores(concurso c){
-
+	if(enJuego(c) && totalConcursantes(c)<=maximoNumeroGanadores(c)){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 
 int probarConcursanteActual(concurso& c, int clave, instante t, int respuesta){
@@ -202,15 +251,42 @@ int probarConcursanteActual(concurso& c, int clave, instante t, int respuesta){
 }
 
 string listarPreguntas(concurso& c){
-
+	return listar(c.grupo);
 }
 
 string listarConcursantes(concurso& c){
-
+	return listar(c.round);
 }
 
 string listarConcurso(concurso& c){
-
+	string texto;
+	texto = "********** ESTADO del CONCURSO **********\n";
+	if(enDoc){
+		texto = texto + "FASE: Documentacion\n";
+	}
+	else if(enInsc){
+		texto = texto + "FASE: Inscripcion\n";
+	}
+	else {
+		texto = texto + "FASE: Juego\n";
+	}
+	texto = texto + "LIMITES maximos-> Pases: " + to_string(c.maximoPases) + " Fallos: " + to_string(c.maximoFallos);
+	texto = texto + " Ganadores: " + to_string(c.maximoNumGanadores);
+	if(enJ){
+		texto = texto + "JUGANDO concursante: ";
+		string concursante;
+		if(obtenerConcursanteActual(c, concursante)==2){
+			texto = texto + concursante;
+		}
+		else{
+			texto = texto + "---";
+		}
+		texto = texto + "\n";
+	}
+	texto = texto + listarPreguntas(c);
+	texto = texto + listarConcursantes(c);
+	texto = texto + "******************************";
+	return texto;
 }
 
 #endif
